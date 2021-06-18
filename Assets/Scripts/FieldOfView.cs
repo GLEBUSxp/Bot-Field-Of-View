628c9e31366cf8e0a6b14bebfa2f5aca0175e08f
GLEBUSxp <glebusxp193@gmail.com>
2021-06-18 20:09:11 +0300

    Publish project

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FieldOfView : MonoBehaviour
+{
+    [Range(0, 360)] public float viewAngle = 90f;
+    public float viewDistance = 15f;
+    public float detectionDistance = 3f;
+    public Transform eye;
+    public Transform target;
+
+    private NavMeshAgent agent;
+    private float rotationSpeed;
+    private Transform agentTransform;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        rotationSpeed = agent.angularSpeed;
+        agentTransform = agent.transform;
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(target.transform.position, agent.transform.position);
+        if (distanceToPlayer <= detectionDistance || IsInView())
+        {
+            RotateToTarget();
+            MoveToTarget();
+        }
+        DrawViewState();
+    }
+
+    private bool IsInView()
+    {
+        float realAngle = Vector3.Angle(eye.forward, target.position - eye.position);
+        RaycastHit hit;
+
+        if(Physics.Raycast(eye.transform.position, target.position - eye.transform.position, out hit, viewDistance))
+        {
+            if (realAngle < viewAngle / 2f && Vector3.Distance(eye.position, target.position) <=viewDistance && hit.transform == target.transform )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void RotateToTarget()
+    {
+        Vector3 lookVector = target.position - agentTransform.position;
+        lookVector.y = 0;
+        
+        if (lookVector == Vector3.zero)
+            return;
+
+        agentTransform.rotation = Quaternion.RotateTowards
+            (
+                agentTransform.rotation,
+                Quaternion.LookRotation(lookVector, Vector3.up),
+                rotationSpeed * Time.deltaTime
+            );
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void DrawViewState()
+    {
+        Vector3 left = eye.position + Quaternion.Euler(new Vector3(0, viewAngle / 2f, 0)) * (eye.forward * viewDistance);
+        Vector3 right = eye.position + Quaternion.Euler(-new Vector3(0, viewAngle / 2f, 0)) * (eye.forward * viewDistance);
+
+        Debug.DrawLine(eye.position, left, Color.yellow);
+        Debug.DrawLine(eye.position, right, Color.yellow);
+
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody rb;
+
+    private void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(horizontalAxis, 0, verticalAxis) * speed;
+
+        rb.velocity = moveVector;
+    }
+}
